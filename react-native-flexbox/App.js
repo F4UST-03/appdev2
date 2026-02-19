4e4927637507aaef89814bdc45950f4d6764f3c5
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-02-20 01:27:13 +0800

    Done with justify content section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -1,9 +1,11 @@
 import Flex from './components/flex';
 import FlexDirection from './components/flexDirection';
 import LayoutDirection from './components/layoutDirection';
+import JustifyContent from './components/justifyContent';
 
 export default function App() {
   // return <Flex />;
   // return <FlexDirection />;
-  return <LayoutDirection />;
+  // return <LayoutDirection />;
+  return <JustifyContent />;
 }
