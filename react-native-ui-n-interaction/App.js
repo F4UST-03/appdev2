aedf0c1bc3c0264d9bb0b55826d96cc5c7bb3c2a
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-02-20 01:23:22 +0800

    Done with Layout Direction section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,17 +1,32 @@
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 
-const FlexDimensionsBasics = () => {
+const PercentageDimensionsBasics = () => {
+  // Try removing the `height: '100%'` on the parent View.
+  // The parent will not have dimensions, so the children can't expand.
   return (
-    // Try removing the `flex: 1` on the parent View.
-    // The parent will not have dimensions, so the children can't expand.
-    // What if you add `height: 300` instead of `flex: 1`? 
-    // - The children will only expand based on the parent's height instead of filling up the entire screen.
-    <View style={{height: 300}}>
-      <View style={{flex: 1, backgroundColor: 'powderblue',}}><Text style={{color: 'black'}}>Box 1</Text></View>
-      <View style={{flex: 2, backgroundColor: 'skyblue',}}><Text style={{color: 'black'}}>Box 2</Text></View>
-      <View style={{flex: 3,backgroundColor: 'steelblue',}}><Text style={{color: 'white'}}>Box 3</Text></View>
+    <View style={{height: '100%'}}>
+      <View
+        style={{
+          height: '15%',
+          backgroundColor: 'powderblue',
+        }}
+      />
+      <View
+        style={{
+          width: '66%',
+          height: '35%',
+          backgroundColor: 'skyblue',
+        }}
+      />
+      <View
+        style={{
+          width: '33%',
+          height: '50%',
+          backgroundColor: 'steelblue',
+        }}
+      />
     </View>
   );
 };
 
-export default FlexDimensionsBasics;
+export default PercentageDimensionsBasics;
