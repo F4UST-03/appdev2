7a8d71ce34b1ea724a5331f644e2d7d6e04631cb
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-02-20 01:37:45 +0800

    Done with align self section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -3,11 +3,13 @@ import FlexDirection from './components/flexDirection';
 import LayoutDirection from './components/layoutDirection';
 import JustifyContent from './components/justifyContent';
 import AlignItems from './components/alignItems';
+import AlignSelf from './components/alignSelf';
 
 export default function App() {
   // return <Flex />;
   // return <FlexDirection />;
   // return <LayoutDirection />;
   // return <JustifyContent />;
-  return <AlignItems />;
+  // return <AlignItems />;
+  return <AlignSelf />;
 }
