e8bfaa38c87645cb6c92de336f73a4c8239e3515
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-02-20 01:30:40 +0800

    Done with align items section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -2,10 +2,12 @@ import Flex from './components/flex';
 import FlexDirection from './components/flexDirection';
 import LayoutDirection from './components/layoutDirection';
 import JustifyContent from './components/justifyContent';
+import AlignItems from './components/alignItems';
 
 export default function App() {
   // return <Flex />;
   // return <FlexDirection />;
   // return <LayoutDirection />;
-  return <JustifyContent />;
+  // return <JustifyContent />;
+  return <AlignItems />;
 }
