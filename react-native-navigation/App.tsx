0f9481035eacbf7ae87cfbc82914c9de25ca3c0b
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-05-08 22:53:19 +0800

    Done with Updating params

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -40,7 +40,7 @@ function DetailsScreen({ route }) {
       <Button
         onPress={
           () =>
-            navigation.push('Details', {
+            navigation.setParams({
               itemId: Math.floor(Math.random() * 100),
             })
         }
